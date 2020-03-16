40a0c0f25e32e3d2458dbd533f31fc22f536d839
Reynaldi Varian <58760029+didi-varian@users.noreply.github.com>
2020-03-16 20:22:19 +0700

    live time

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _timeString;
+
+  @override
+  void initState() {
+    _timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            // Container(
+            // color: Colors.white,
+            // padding: EdgeInsets.zero,
+            // margin: EdgeInsets.zero,
+            // child:
+            Text(
+              _timeString,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 540,
+                height: 0.8
+              ),
+            ),
+            // ),
+            // Container(
+            // color: Colors.red,
+            // padding: EdgeInsets.zero,
+            // margin: EdgeInsets.zero,
+            // child:
+            SecondTime(),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('hh:mm').format(dateTime);
+  }
+}
+
+class SecondTime extends StatefulWidget {
+  @override
+  _SecondTimeState createState() => _SecondTimeState();
+}
+
+class _SecondTimeState extends State<SecondTime> {
+  String _timeString;
+
+  @override
+  void initState() {
+    _timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _timeString,
+      style: TextStyle(
+        color: Colors.yellow,
+        fontSize: 240,
+        height: 0.8
+      ),
+    );
+  }
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('ss').format(dateTime);
+  }
+}
